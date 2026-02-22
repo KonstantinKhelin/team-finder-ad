7c8be4b31c8587521b3b2a9847936d70d2798e8c
Konstantin Khelin <kostya.xelin.07@mail.ru>
2026-02-22 14:01:42 +0500

    correct urls team_finder

--- team_finder/urls.py
+++ team_finder/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', (admin.site.urls)),
     path('', include('projects.urls')),
-    path('users/', include('users.urls')
+    path('users/', include('users.urls'))
 ]
